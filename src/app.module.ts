acd381e9443ef698e80aa466071e925d5dd5023b
devlopersabbir <devlopersabbir@gmail.com>
2025-08-26 12:32:10 +0600

    [issue 😁] bull que issues 👨‍🍼

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
-import { BullModule } from "@nestjs/bullmq";
-import { CacheModule } from "@nestjs/cache-manager";
 import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ConfigModule } from "@nestjs/config";
 import { EventEmitterModule } from "@nestjs/event-emitter";
 import { PassportModule } from "@nestjs/passport";
 import { ScheduleModule } from "@nestjs/schedule";
 import { AppController } from "./app.controller";
-import { ENVEnum } from "./common/enum/env.enum";
+import BullConfig from "./configs/bull.config";
+import CacheConfig from "./configs/redis.store";
 import { LibModule } from "./lib/lib.module";
 import { NotificationModule } from "./lib/notification/notification.module";
 import { MainModule } from "./main/main.module";
@@ -16,26 +15,10 @@ import { MainModule } from "./main/main.module";
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    CacheModule.register({
-      isGlobal: true,
-    }),
+    CacheConfig,
+    BullConfig,
     EventEmitterModule.forRoot(),
     ScheduleModule.forRoot(),
-    BullModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        const host = configService.getOrThrow<string>(ENVEnum.REDIS_HOST);
-        const port = configService.getOrThrow<string>(ENVEnum.REDIS_PORT);
-
-        return {
-          connection: {
-            host,
-            port: parseInt(port, 10),
-          },
-        };
-      },
-    }),
     NotificationModule,
     PassportModule,
     LibModule,
@@ -43,7 +26,7 @@ import { MainModule } from "./main/main.module";
   ],
   controllers: [AppController],
 })
-export class AppModule {}
+export class AppModule { }
 // export class AppModule implements NestModule {
 //   // configure(consumer: MiddlewareConsumer) {
 //   //   consumer.apply(LoggerMiddleware).forRoutes('*');
