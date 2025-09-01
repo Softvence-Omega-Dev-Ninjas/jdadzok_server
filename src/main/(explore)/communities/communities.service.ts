8285292807f6f3601f4ea44d9d56deb842246952
devlopersabbir <devlopersabbir@gmail.com>
2025-09-01 09:15:31 +0600

    [fix] deployement issues

--- src/main/(explore)/communities/communities.service.ts
+++ src/main/(explore)/communities/communities.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from "@nestjs/common";
-import { PrismaService } from "@project/lib/prisma/prisma.service";
 import { CreateCommunityDto } from "./dto/communities.dto";
 
 @Injectable()
 export class CommunitiesService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor() {}
 
   async createCommunity(userId: string, dto: CreateCommunityDto) {
-    console.log(userId, dto);
+    console.info(userId, dto);
     return "Create new community";
   }
 }
