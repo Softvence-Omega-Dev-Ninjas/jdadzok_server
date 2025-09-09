3160346e8ec183c7885620173ba5f548d1080b1d
devlopersabbir <devlopersabbir@gmail.com>
2025-09-09 12:52:41 +0600

    changed s3

--- src/s3/s3.service.ts
+++ src/s3/s3.service.ts
@@ -8,11 +8,16 @@
 // @Injectable()
 // export class S3Service {
 //     private s3: S3Client;
+//     private AWS_S3_BUCKET_NAME: string;
 
 //     constructor(private readonly configSerivce: ConfigService) {
 //         const AWS_REGION = configSerivce.getOrThrow(ENVEnum.AWS_REGION)
 //         const AWS_ACCESS_KEY_ID = configSerivce.getOrThrow(ENVEnum.AWS_ACCESS_KEY_ID)
 //         const AWS_SECRET_ACCESS_KEY = configSerivce.getOrThrow(ENVEnum.AWS_SECRET_ACCESS_KEY)
+//         const AWS_S3_BUCKET_NAME = configSerivce.getOrThrow(ENVEnum.AWS_S3_BUCKET_NAME)
+//         this.AWS_S3_BUCKET_NAME = AWS_S3_BUCKET_NAME
+
+
 //         this.s3 = new S3Client({
 //             region: AWS_REGION!,
 //             credentials: {
@@ -27,7 +32,7 @@
 //         const key = `${folder}/${uuid()}${fileExtension}`;
 
 //         const command = new PutObjectCommand({
-//             Bucket: process.env.AWS_S3_BUCKET_NAME,
+//             Bucket: this.AWS_S3_BUCKET_NAME,
 //             Key: key,
 //             Body: file.buffer,
 //             ContentType: file.mimetype,
