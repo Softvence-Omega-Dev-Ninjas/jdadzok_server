90c743b59ed27138cab26297a02ef723d42ecf83
ranamiah221 <ranarasul21@gmail.com>
2025-09-16 06:22:37 +0600

    change host name

--- src/main.ts
+++ src/main.ts
@@ -61,7 +61,7 @@ async function bootstrap() {
 
   await app.listen(
     port,
-    process.env.NODE_ENV === "development" ? "0.0.0.0" : "localhost",
+    "localhost",
     () => console.info(`PORT=${port}`),
   );
 }
