a6d9f26ab6b634e1087f49b97290ddbde9fa3029
devlopersabbir <devlopersabbir@gmail.com>
2025-09-22 23:57:09 +0600

    with v0.4.7

---
+++ src/main/(posts)/likes/getway/like.getway.ts
@@ -0,0 +1,13 @@
+import { BaseSocketGateway } from "@module/(sockets)/base/abstract-socket.gateway";
+import { WebSocketGateway } from "@nestjs/websockets";
+import { JwtServices } from "@project/services/jwt.service";
+import { LikeService } from "../like.service";
+
+@WebSocketGateway({
+    namespace: "like"
+})
+export class LikeGetway extends BaseSocketGateway {
+    constructor(private readonly likeService: LikeService, private readonly jwtServices: JwtServices) {
+        super(jwtServices)
+    }
+}
