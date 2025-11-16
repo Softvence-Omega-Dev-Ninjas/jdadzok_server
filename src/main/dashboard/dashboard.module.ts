f53739fe4d2e89e69382b4af74aa98b36185dac2
ranamiah221 <ranarasul21@gmail.com>
2025-11-16 06:14:41 +0600

    added event api based on volunteer project

--- src/main/dashboard/dashboard.module.ts
+++ src/main/dashboard/dashboard.module.ts
@@ -3,9 +3,11 @@ import { DashboardController } from "./controller/dashboard.controller";
 import { DashboardService } from "./service/dashboard.service";
 import { CommunityNgoController } from "./controller/communityNgo.controller";
 import { CommunityNgoService } from "./service/communityNgo.service";
+import { EventController } from "./controller/event.controller";
+import { EventService } from "./service/event.service";
 
 @Module({
-    controllers: [DashboardController, CommunityNgoController],
-    providers: [DashboardService, CommunityNgoService],
+    controllers: [DashboardController, CommunityNgoController, EventController],
+    providers: [DashboardService, CommunityNgoService, EventService],
 })
 export class DashboardModule {}
