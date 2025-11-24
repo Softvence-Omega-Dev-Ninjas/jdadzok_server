bb25854ec0037a689e2c1e302dc34479c2377feb
ranamiah221 <ranarasul21@gmail.com>
2025-11-25 02:22:40 +0600

    added post featured

---
+++ src/main/(posts)/post-featured/post.featured.controller.ts
@@ -0,0 +1,46 @@
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { ApiBearerAuth, ApiOperation } from "@nestjs/swagger";
+import { PostFeaturedService } from "./post.featured.service";
+import { GetVerifiedUser } from "@common/jwt/jwt.decorator";
+import { VerifiedUser } from "@type/shared.types";
+import { SavePostDto } from "./dto/save.post.dto";
+import { handleRequest } from "@common/utils/handle.request.util";
+
+@Controller("post-featured")
+@ApiBearerAuth()
+export class PostFeaturedController {
+    constructor(private service: PostFeaturedService) {}
+
+    @Post("/save")
+    @ApiOperation({ summary: "Save a post" })
+    async save(@GetVerifiedUser() user: VerifiedUser, @Body() dto: SavePostDto) {
+        return handleRequest(
+            () => this.service.savePost(user.id, dto.postId),
+            "Post saved successfully",
+        );
+    }
+
+    @Delete("/unsave")
+    @ApiOperation({ summary: "Unsave a post" })
+    async unsave(@GetVerifiedUser() user: VerifiedUser, @Body() dto: SavePostDto) {
+        return handleRequest(
+            () => this.service.unsavePost(user.id, dto.postId),
+            "Post unsaved successfully",
+        );
+    }
+
+    @Get("status/:postId")
+    @ApiOperation({ summary: "Check if post is saved" })
+    async checkSaved(@GetVerifiedUser() user: VerifiedUser, @Param("postId") postId: string) {
+        return handleRequest(() => this.service.isSaved(user.id, postId), "Saved status fetched");
+    }
+
+    @Get("my")
+    @ApiOperation({ summary: "Get all saved posts of the user" })
+    async mySaved(@GetVerifiedUser() user: VerifiedUser) {
+        return handleRequest(
+            () => this.service.getMySavedPosts(user.id),
+            "Saved posts fetched successfully",
+        );
+    }
+}
