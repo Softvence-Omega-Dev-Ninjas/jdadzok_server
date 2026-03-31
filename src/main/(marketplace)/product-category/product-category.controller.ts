1440be29a0cecc4569049f1e254b9c94329051f4
ranamiah221 <ranarasul21@gmail.com>
2026-04-01 00:26:58 +0600

    added crud operation for product category

--- src/main/(marketplace)/product-category/product-category.controller.ts
+++ src/main/(marketplace)/product-category/product-category.controller.ts
@@ -1,11 +1,12 @@
 import { handleRequest } from "@common/utils/handle.request.util";
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { CreateProductCategoryDto } from "./dto/create-product-category.dto";
+import { UpdateProductCategoryDto } from "./dto/update-product-category.dto";
 import { ProductCategoryService } from "./product-category.service";
 
 @Controller("product-category")
 export class ProductCategoryController {
-    constructor(private readonly service: ProductCategoryService) {}
+    constructor(private readonly service: ProductCategoryService) { }
 
     @Post("")
     async create(@Body() dto: CreateProductCategoryDto) {
@@ -16,11 +17,30 @@ export class ProductCategoryController {
         return handleRequest(() => this.service.findAll(), "Get All Product Category Successfully");
     }
 
-    // @Delete(':id')
-    // async remove(@Param('id') id:string){
-    //      return handleRequest(
-    //         () => this.service.remove(id),
-    //         "Delete Product Category Successfully",
-    //     );
-    // }
+    @Get(':id')
+    async findOne(@Param('id') id: string) {
+        return handleRequest(
+            () => this.service.findOne(id),
+            'Get Product Category Successfully',
+        );
+    }
+
+    @Patch(':id')
+    async update(
+        @Param('id') id: string,
+        @Body() dto: UpdateProductCategoryDto,
+    ) {
+        return handleRequest(
+            () => this.service.update(id, dto),
+            'Update Product Category Successfully',
+        );
+    }
+
+    @Delete(':id')
+    async remove(@Param('id') id: string) {
+        return handleRequest(
+            () => this.service.remove(id),
+            'Delete Product Category Successfully',
+        );
+    }
 }
